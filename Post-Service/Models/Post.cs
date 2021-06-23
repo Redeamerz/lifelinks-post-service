d3f3536e7ceb2c1ae450850ccebe16cd17232462
Wesley Haagmans <wesleytuuh@gmail.com>
2021-06-23 19:14:41 +0200

    ~UPDATE: Code maid cleanup

--- Post-Service/Models/Post.cs
+++ Post-Service/Models/Post.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
-
-namespace Posting_Service.Models
+﻿namespace Posting_Service.Models
 {
-    public class Post
-    {
-        public virtual string id { get; set; }
-        public virtual string userId { get; set; }
-        public virtual string postContent { get; set; }
-        public virtual string username { get; set; }
-        public virtual long likes { get; set; }
-
-    }
+	public class Post
+	{
+		public virtual string id { get; set; }
+		public virtual string userId { get; set; }
+		public virtual string postContent { get; set; }
+		public virtual string username { get; set; }
+		public virtual long likes { get; set; }
+	}
 }
